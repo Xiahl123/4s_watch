3e47a58d22e9789e382848efee5dcf30a356d00e
wancmx <15588030768@163.com>
2024-11-29 17:20:42 +0800

    alter2

--- 4s_4G_watch.py
+++ 4s_4G_watch.py
@@ -6,9 +6,10 @@ HOST = '127.0.0.1'  # 本地地址
 PORT = 7998          # 监听的端口
 
 # 创建一个 TCP/IP 套接字
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
     # 绑定套接字到地址
     server_socket.bind((HOST, PORT))
+    print("开始监听")
     while True:
         # 接收数据:
         data, addr = server_socket.recvfrom(1024) # 最大字节数
