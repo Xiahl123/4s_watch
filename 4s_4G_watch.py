b650985761dae0392aa2a0ee2648e405c0a566d6
wancmx <15588030768@163.com>
2024-11-29 17:18:14 +0800

    alter2

--- 4s_4G_watch.py
+++ 4s_4G_watch.py
@@ -9,19 +9,9 @@ PORT = 7998          # 监听的端口
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     # 绑定套接字到地址
     server_socket.bind((HOST, PORT))
-    
-    # 开始监听连接
-    server_socket.listen()
-    print(f"服务器正在 {HOST}:{PORT} 等待连接...")
-
-    # 接受连接
-    conn, addr = server_socket.accept()
-    with conn:
-        print(f"已连接到 {addr}")
-        while True:
-            # 接收数据
-            data = conn.recv(1024)
-            if not data:
-                break  # 如果没有数据则退出循环
-            print(f"收到数据: {data.decode('utf-8')}")
+    while True:
+        # 接收数据:
+        data, addr = server_socket.recvfrom(1024) # 最大字节数
+        dataStr = data.decode("utf-8")
+        print("数据:",dataStr)
 
